1a0db679d908f023ddcb54eadc3196d89a7df52f
atulvaru <atulk2512001@gmail.com>
2024-07-04 10:55:52 +0530

    syncing changes

---
+++ utils/footerData.js
@@ -0,0 +1,37 @@
+// utils/utils.js
+export const categories = [
+    "Zeneta Fashion",
+    "Xgard 304",
+    "Never Star",
+    "Rillingen Hood",
+    "DropBrain",
+    "Red Pride",
+    "Onedry 1998",
+    "Starclothing",
+    "Zeneta Fashion",
+    "Xgard 304",
+    "Never Star",
+    "Rillingen Hood",
+    "DropBrain",
+    "Red Pride",
+    "Onedry 1998",
+    "Starclothing",
+    "Starclothing2",
+  ];
+  
+  export const footerInfo = {
+    title: "Ridhi Suman Fabric",
+    description: `
+      Aims to make it easier for every community in the world to carry out various buying and selling transactions online.
+      It is one of the world's online buying and selling sites whose development is relatively fast.
+      You can sell products online at the slabshop besides being able to enjoy the process of buying various products more quickly and effectively.
+      You can sign up for the exclusive slabshop Seller community if you want to launch your own business.
+    `,
+    copyright: "Ridhi Suman © 2024-2025, All Rights Reserved",
+    links: [
+      { href: "#", text: "Privacy Policy" },
+      { href: "#", text: "Terms Of Use" },
+      { href: "#", text: "FAQ" },
+    ],
+  };
+  
