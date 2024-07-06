df39ba2e108fbdb4da97f3ed830f01732a0d9fd1
atulvaru <atulk2512001@gmail.com>
2024-07-06 17:41:47 +0530

    created checkout page

--- components/cardTitle/CardTitle..js
+++ components/cardTitle/CardTitle..js
@@ -1,12 +1,27 @@
-import Image from 'next/image'
-import React from 'react'
-const CardTitle = (props) => {
-  return (
-    <div className='flex items-center border-b-2 border-b-[#0052D4] h-14 p-4 gap-2'>
-        <Image src={props.data.icon} width={15} height={15}/>
-        <span>{props.data.name}</span>
-    </div>
-  )
-}
+import Image from "next/image";
+import React from "react";
 
-export default CardTitle
+const CardTitle = ({ data, size, borderColor }) => {
+  const borderColorClass = borderColor || "#0052D4";
+  const textSize = size === "small" ? "text-md" : "text-lg";
+
+
+  return (
+    <div
+      className={`flex items-center h-14 py-3 mb-4 gap-2 border-b-2`}
+      style={{ borderColor: borderColorClass }}
+    >
+      <Image
+        src={data.icon}
+        width={size === "small" ? 20 : 25}
+        height={size === "small" ? 12 : 20}
+        alt={data.name}
+      />
+      <span className={`${textSize} font-semibold  `}>
+        {data.name}
+      </span>
+    </div>
+  );
+};
+
+export default CardTitle;
