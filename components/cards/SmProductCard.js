24a40a29db0860e899fdaa59acb32a11ebdfdc41
Musharraf Jamal <musharrafjamal08@gmail.com>
2024-07-20 12:42:51 +0530

    Restoring card component

---
+++ components/cards/SmProductCard.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+import AddToCartButton from "./AddToCartButton";
+import IncrementDecrementButton from "../button/IncrementDecrementButton";
+
+const SmProductCard = ({ product, check }) => {
+  return (
+    <div className="bg-white border-2 rounded-lg my-2 overflow-hidden">
+      <div className="flex items-center p-2 gap-4">
+        <Image
+          src="/icon/ProductImage.png"
+          width={90}
+          height={50}
+          className="rounded-lg mr-4"
+          alt="Product"
+        />
+        <div className="flex flex-col gap-1">
+          <h3 className="text-md font-semibold">{product.title}</h3>
+          <p className="text-sm text-[#11998E]">{product.discountedPrice}</p>
+          {check != "userProfile" && (
+            <p className="text-sm text-gray-500">Quantity: 2</p>
+          )}
+
+          {check === "userProfile" && <AddToCartButton />}
+
+          {check === "checkout" && <IncrementDecrementButton />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SmProductCard;
